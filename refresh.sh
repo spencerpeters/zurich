fa29d2835d163c116e60665ba0404929557cda52
spencerpeters <spencer@goebelpeters.com>
2017-10-22 18:53:52 +0200

    made everything look like the old blog

--- refresh.sh
+++ refresh.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 stack exec site build
+git add .
+git commit -am "Refreshed site from script."
+git push
 stack exec site watch
--- site.hs
+++ site.hs
@@ -15,15 +15,12 @@ import           Control.Monad (liftM, (<=<), foldM)
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyllWith config $ do
-    let product = [(++ "images/*"), (++ "posts/*/images/*")] <*> ["", "zurich/"] in
-        match (unionPatterns product) $ do
+
+    match ["images/*"] $ do
             route   idRoute
-            compile copyFileCompiler
-            --removed this since rebuilds were taking too long.
-            -- Might need to manually resize images later.
-            --let args = ["-", "-resize", "200000@", "-"] in
-            --    compile $ getResourceLBS >>= withItemBody (unixFilterLBS "convert" args)
-                -- resize to 200,000 px
+            let args = ["-", "-resize", "200000@", "-"] in
+               compile $ getResourceLBS >>= withItemBody (unixFilterLBS "convert" args)
+--                resize to 200,000 px
 
     match "css/*" $ do
         route   idRoute
@@ -35,8 +32,7 @@ main = hakyllWith config $ do
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
-    let product = [(++ "posts/*")] <*> ["", "zurich/"] in
-        match (unionPatterns product) $ do
+        match ["posts/*"] $ do
             route $ setExtension "html"
             compile postCompiler
 
@@ -49,43 +45,70 @@ main = hakyllWith config $ do
 -- --             route $ (setExtension "html") `composeRoutes` (customRoute remIntermediateDir)
 --             compile postCompiler
 
-    let createArchive subdir =
-            create [fromFilePath $ subdir ++ "archive.html"] $ do
-                route idRoute
-                compile $ do
-                    posts <- recentFirst =<< loadAll (fromGlob $ subdir ++ "posts/*")
-                    let archiveCtx =
-                            listField "posts" postCtx (return posts) `mappend`
-                            constField "title" "Archives"            `mappend`
-                            defaultContext
+    create ["archive.html"] $ do
+        route idRoute
+            compile $ do
+                posts <- recentFirst =<< loadAll (fromGlob $ subdir ++ "posts/*")
+                let archiveCtx =
+                        listField "posts" postCtx (return posts) `mappend`
+                        constField "title" "Archives"            `mappend`
+                        defaultContext
 
-                    makeItem ""
-                        >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                        >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                        >>= relativizeUrls
+                makeItem ""
+                    >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+                    >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                    >>= relativizeUrls
 
-    foldl1 (>>) $ map createArchive ["", "zurich/"]
+--     let createArchive subdir =
+--             create [fromFilePath $ subdir ++ "archive.html"] $ do
+--                 route idRoute
+--                 compile $ do
+--                     posts <- recentFirst =<< loadAll (fromGlob $ subdir ++ "posts/*")
+--                     let archiveCtx =
+--                             listField "posts" postCtx (return posts) `mappend`
+--                             constField "title" "Archives"            `mappend`
+--                             defaultContext
+--
+--                     makeItem ""
+--                         >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+--                         >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+--                         >>= relativizeUrls
+--
+--     foldl1 (>>) $ map createArchive ["", "zurich/"]
+
+    match "index.html" $ do
+        route idRoute
+        compile $ do
+            posts <- take5OfRecentFirst =<< loadAll (fromGlob $ subdir ++ "posts/*")
+            let indexCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    constField "title" "Home"                `mappend`
+                    defaultContext
+
+            getResourceBody
+                >>= applyAsTemplate indexCtx
+                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= relativizeUrls
+
+--     let matchIndex subdir =
+--             match (fromGlob $ subdir ++ "index.html") $ do
+--                     route idRoute
+--                     compile $ do
+--                         posts <- take5OfRecentFirst =<< loadAll (fromGlob $ subdir ++ "posts/*")
+--                         let indexCtx =
+--                                 listField "posts" postCtx (return posts) `mappend`
+--                                 constField "title" "Home"                `mappend`
+--                                 defaultContext
+--
+--                         getResourceBody
+--                             >>= applyAsTemplate indexCtx
+--                             >>= loadAndApplyTemplate "templates/default.html" indexCtx
+--                             >>= relativizeUrls
+--
+--     foldl1 (>>) $ map matchIndex ["", "zurich/"]
 
 
-    let matchIndex subdir =
-            match (fromGlob $ subdir ++ "index.html") $ do
-                    route idRoute
-                    compile $ do
-                        posts <- take5OfRecentFirst =<< loadAll (fromGlob $ subdir ++ "posts/*")
-                        let indexCtx =
-                                listField "posts" postCtx (return posts) `mappend`
-                                constField "title" "Home"                `mappend`
-                                defaultContext
-
-                        getResourceBody
-                            >>= applyAsTemplate indexCtx
-                            >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                            >>= relativizeUrls
-
-    foldl1 (>>) $ map matchIndex ["", "zurich/"]
-
-
-    match ("templates/*" .||. "zurich/templates/*") $ do
+    match "templates/*" $ do
         route idRoute
         compile templateBodyCompiler
 
@@ -123,7 +146,7 @@ myWriterOptions = defaultHakyllWriterOptions {
 
 take5OfRecentFirst = (liftM (take 5)) .  recentFirst
 
-unionPatterns patterns = foldl1 (.||.) (fmap fromGlob patterns)
+-- unionPatterns patterns = foldl1 (.||.) (fmap fromGlob patterns)
 
 postCompiler = pandocCompilerWith myReaderOptions myWriterOptions
                 >>= loadAndApplyTemplate "templates/post.html" postCtx
