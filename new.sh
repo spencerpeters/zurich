f0e056315040e61fede71c6315d548a15e7747e6
Spencer Peters <spencer@goebelpeters.com>
2020-06-14 00:49:30 -0700

    Refreshed site from script.

--- new.sh
+++ new.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-
+if [ $# -ne 1 ];
+then echo "Usage: ./new.sh title"
+exit 1
+fi 
 DATE=`date +%F`
 TITLEWITHSPACES=$1
 RESULT="---\ntitle: $TITLEWITHSPACES\nauthor: Spencer\n---"
 FILETITLE=${TITLEWITHSPACES// /-}
 echo -e $RESULT >> "./posts/$DATE-$FILETITLE.markdown"
-# sublime "./posts/$DATE-$FILETITLE.markdown"
+#emacs "./posts/$DATE-$FILETITLE.markdown"
+sublime "./posts/$DATE-$FILETITLE.markdown"
